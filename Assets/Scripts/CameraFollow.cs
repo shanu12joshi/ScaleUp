63b45d34f09cd23b8dc481b4b5bff812b97a9f12
Shanu Joshi <shanujoshi@Shanus-MacBook-Pro.local>
2021-04-09 15:00:26 +0530

    first commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float CameraFollowSpeed = 0.125f;
+
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desPosition = player.position + offset;
+
+        Vector3 newPosition = Vector3.Lerp(transform.position,desPosition,CameraFollowSpeed * Time.deltaTime);
+
+        transform.position = newPosition;
+        transform.LookAt(player);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   float speed = 0.1f;
+    private Touch touch;
+    private float valueToIncrease = 0.2f;
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z+0.4f);
+        
+        if(Input.touchCount > 0)
+        {
+            touch= Input.GetTouch(0);
+        
+            if(touch.phase == TouchPhase.Moved )
+            {
+        
+                transform.position = new Vector3(transform.position.x+touch.deltaPosition.x * speed,transform.position.y,transform.position.z);
+            }
+        }
+
+        Vector3 pos = transform.position;
+
+        pos.x = Mathf.Clamp(transform.position.x,-4f,+4f);
+
+        transform.position = pos;
+        
+    }
+    void FixedUpdate(){
+        if(Input.GetKey("a")){
+            transform.position = new Vector3(transform.position.x - speed * speed,transform.position.y,transform.position.z);
+        }
+        if(Input.GetKey("d")){
+            transform.position = new Vector3(transform.position.x + speed * speed,transform.position.y,transform.position.z);
+        }
+    }
+    void OnCollisionEnter(Collision col){
+        if(gameObject.GetComponent<MeshRenderer>().material.name == col.gameObject.GetComponent<MeshRenderer>().material.name)
+        {
+            Destroy(col.gameObject);
+            ScaleUp();
+            
+        } 
+        else{
+            Destroy(col.gameObject);
+            ScaleDown();
+        }
+        
+    }
+
+    private void ScaleUp(){
+        // transform.localScale += new Vector3(0.1f,0.1f,0.1f);
+        transform.localScale += new Vector3(valueToIncrease,valueToIncrease,valueToIncrease);
+        transform.position= new Vector3(transform.position.x,transform.position.y+valueToIncrease, transform.position.z);
+    }
+
+    private void ScaleDown(){
+        transform.localScale -= new Vector3(valueToIncrease,valueToIncrease,valueToIncrease);
+        transform.position= new Vector3(transform.position.x,transform.position.y-valueToIncrease, transform.position.z);
+    }
+}
